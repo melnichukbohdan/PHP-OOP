c7cd01ade32b75647f248e94d85d94d2064f1bc3
melnichukbohdan <melnichukbogdan1@gmail.com>
2021-11-26 18:14:56 +0200

    PHP OOP Lab 1.4

--- Users.php
+++ Users.php
@@ -2,10 +2,18 @@
 
 class Users {
 
+    public static $userCounter = 0;
+
     public function __construct($name, $login,$password) {
         $this->name = $name;
         $this->login = $login;
         $this->password = $password;
+        ++self::$userCounter;
+    }
+
+    //this method adds to the total number of objects, including cloned objects
+    public function __clone() {
+        ++self::$userCounter;
     }
 
     public function showInfo () {
@@ -19,29 +27,48 @@ class Users {
     }
 }
 
-
-$user1 = new Users('Jom', 'admin', 'Bos12345');
-$user2 = new Users('Mike', 'dodik', 'pas12345');
-$user3 = new Users('Bob', 'Bob123', 'bob3214');
-
-
-$user1->showInfo();
-$user2->showInfo();
-$user3->showInfo();
-
-
 class SuperUser extends Users {
+
+    public static $superUserCounter = 0;
 
     public function __construct($name, $login, $password, $role) {
         parent::__construct($name, $login, $password);
         $this->role = $role;
+        ++self::$superUserCounter;
+        --parent::$userCounter;
+    }
+
+    //this method adds to the total number of objects, including cloned objects
+    public function __clone() {
+        ++self::$superUserCounter;
+
     }
 
     public function showInfo() {
         echo 'User role' . ' ' . '-' . ' ' . $this->role . "<br>";
     }
+
+
 }
 
-$user = new SuperUser('Jim', 'userGod12', 'goD123098', 'admin');
 
-$user->showInfo();
+
+$user1 = new Users('Jom', 'admin', 'Bos12345');
+$user2 = new Users('Mike', 'dodik', 'pas12345');
+$user3 = new Users('Bob', 'Bob123', 'bob3214');
+$user4 = clone $user3;
+$user5 = clone $user1;
+
+$user1->showInfo();
+$user2->showInfo();
+$user3->showInfo();
+
+$sUser1 = new SuperUser('Jim', 'userGod12', 'goD123098', 'admin');
+$sUser2 = clone $sUser1;
+
+
+$sUser1->showInfo();
+
+
+echo Users::$userCounter . "<br>";
+echo SuperUser::$superUserCounter . "<br>";
